b662d14c64560e8830e201b5af756a87c7a2d68c
EDB\ahmed.tagalsir <ahmed.alhassen.tagalsir@gmail.com>
2022-01-05 15:30:24 +0200

    dropdown + topNav

--- src/components/Dropdown/Dropdown.jsx
+++ src/components/Dropdown/Dropdown.jsx
@@ -1,8 +1,51 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./Dropdown.css";
 
-function Dropdown() {
-  return <div></div>;
+const clickOutsideRef = (content_ref, toggle_ref) => {
+  document.addEventListener("mousedown", (e) => {
+    //user click toggle
+    if (toggle_ref.current && toggle_ref.current.contains(e.target)) {
+      content_ref.current.classList.toggle("active");
+    } else {
+      //user click outside toggle and content
+      if (content_ref.current && !content_ref.current.contains(e.target)) {
+        content_ref.current.classList.remove("active");
+      }
+    }
+  });
+};
+
+function Dropdown({
+  icon,
+  badge,
+  customToggle,
+  contentData,
+  renderItems,
+  renderFooter,
+}) {
+  const dropdown_content_el = useRef(null);
+  const dropdown_toggle_el = useRef(null);
+  clickOutsideRef(dropdown_content_el, dropdown_toggle_el);
+
+  return (
+    <div className="dropdown">
+      <button ref={dropdown_toggle_el} className="dropdown__toggle">
+        {icon ? <i className={icon}></i> : ""}
+        {badge ? <span className="dropdown__toggle-badge">{badge}</span> : ""}
+        {customToggle ? customToggle : ""}
+      </button>
+      <div ref={dropdown_content_el} className="dropdown__content">
+        {contentData && renderItems
+          ? contentData.map((item, index) => renderItems(item, index))
+          : ""}
+        {renderFooter ? (
+          <div className="dropdown__footer">{renderFooter()}</div>
+        ) : (
+          ""
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default Dropdown;
--- src/components/TopNav/TopNav.jsx
+++ src/components/TopNav/TopNav.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import Dropdown from "../Dropdown/Dropdown";
 import "./TopNav.css";
+import notifactions from "../../assets/JsonData/notification.json";
+import { Link } from "react-router-dom";
+
+const renderNotificationItem = (item, index) => (
+  <div className="notification-item" key={index}>
+    <i className={item.icon}></i>
+    <span>{item.content}</span>
+  </div>
+);
 
 const TopNav = () => {
   return (
@@ -9,9 +19,24 @@ const TopNav = () => {
         <i className="bx  bx-search"></i>
       </div>
       <div className="topnav__right">
-        <div className="topnav__right-item">{/* dropdown here  */}</div>
-        <div className="topnav__right-item">{/* dropdown here  */}</div>
-        <div className="topnav__right-item">{/* theme settings  */}</div>
+        <div className="topnav__right-item">
+          <Dropdown icon="bx bx-user" />
+          {/* dropdown here  */}
+        </div>
+        <div className="topnav__right-item">
+          <Dropdown
+            icon="bx bx-bell"
+            badge="12"
+            contentData={notifactions}
+            renderItems={(item, index) => renderNotificationItem(item, index)}
+            renderFooter={() => <Link to="/">View All</Link>}
+          />
+          {/* dropdown here  */}
+        </div>
+        <div className="topnav__right-item">
+          <Dropdown />
+          {/* dropdown here  */}
+        </div>
       </div>
     </div>
   );
