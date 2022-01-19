c4b95e0ccf21cdbab9efa7444ef68bc30f5ffae2
EDB\ahmed.tagalsir <ahmed.alhassen.tagalsir@gmail.com>
2022-01-19 15:25:41 +0200

    status card update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import statusCards from "../assets/JsonData/status-card-data.json";
+import StatusCard from "../components/Status-card/StatusCard";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <h2 className="page-header">Dashboard</h2>
+      <div className="row">
+        <div className="col-6">
+          <div className="row">
+            {statusCards.map((item, index) => (
+              <div className="col-6">
+                {/* status card here  */}
+
+                <StatusCard
+                  icon={item.icon}
+                  count={item.count}
+                  title={item.title}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
